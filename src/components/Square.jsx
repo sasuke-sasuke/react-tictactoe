35b408da7d090e06d2ff3dc5a37bf9249b75683b
Jeof Petty <jeofpetty@icloud.com>
2023-06-06 20:21:05 -0400

    installed sass. created board and sqaure component barebones.

---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+const Square = ({ value }) => {
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
+};
+
+export default Square;
